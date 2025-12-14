d787b619959b61a8f3681d67a65e9a0ab9317a39
Lilvamp237 <107618999+Lilvamp237@users.noreply.github.com>
2025-12-14 16:08:06 +0530

    publications certifications updated

--- constants.ts
+++ constants.ts
@@ -52,33 +52,64 @@ export const CERTIFICATIONS: Certification[] = [
     name: "GitHub Copilot",
     provider: "Microsoft",
     date: "Dec 2025",
-    badgeUrl: "https://ui-avatars.com/api/?name=D+L&background=0284c7&color=fff&size=128&bold=true",
+    badgeUrl: "/badges/copilot.png",
     verificationLink: "https://learn.microsoft.com/api/credentials/share/en-us/Sumudu05-1888/7E05411E38180AC0?sharingId"
   },
   {
     id: "c2",
-    name: "LangChain & LLM App Dev",
-    provider: "DeepLearning.AI",
-    date: "2023",
-    badgeUrl: "https://ui-avatars.com/api/?name=L+C&background=16a34a&color=fff&size=128&bold=true",
-    verificationLink: "https://www.deeplearning.ai/short-courses/langchain-for-llm-application-development/"
+    name: "Oracle Cloud Infrastructure 2025 Certified AI Foundations Associate",
+    provider: "Oracle",
+    date: "Sep 2025",
+    badgeUrl: "/badges/OCI25AICFA.jpeg",
+    verificationLink: "https://catalog-education.oracle.com/pls/certview/sharebadge?id=BCA92DFDF1EE5C98047C5CD7EF1090710F40E633B651CD271D84425D0981BF90"
   },
   {
     id: "c3",
-    name: "Unity Certified Associate",
-    provider: "Unity Technologies",
-    date: "2022",
-    badgeUrl: "https://ui-avatars.com/api/?name=U+T&background=000000&color=fff&size=128&bold=true",
-    verificationLink: "https://unity.com/products/unity-certifications"
+    name: "Intro to Machine Learning",
+    provider: "Kaggle",
+    date: "Aug 2025",
+    badgeUrl: "/badges/kaggle.jpeg",
+    verificationLink: "https://www.kaggle.com/learn/certification/sumuu237/intro-to-machine-learning"
   },
   {
     id: "c4",
-    name: "Machine Learning Specialization",
-    provider: "Coursera (Stanford)",
-    date: "2022",
-    badgeUrl: "https://ui-avatars.com/api/?name=S+U&background=ca8a04&color=fff&size=128&bold=true"
-    // No verification link for this one (example case)
+    name: "Unity Essentials Pathway",
+    provider: "Unity",
+    date: "Jul 2025",
+    badgeUrl: "/badges/unity-essentials-pathway.png",
+    verificationLink: "https://www.credly.com/badges/20d7c767-4e53-460c-8d4c-192b02759373/linked_in_profile"
+  },
+  {
+    id: "c5",
+    name: "Postman API Fundamentals Student Expert",
+    provider: "Postmanapi",
+    date: "Feb 2025",
+    badgeUrl: "badges/postman.jpeg",
+    verificationLink: "https://api.badgr.io/public/assertions/yKS5WPrYQJC1nIIOwBfPUw"    
+  },
+  {
+    id: "c6",
+    name: "Microsoft Certified: Azure AI Fundamentals",
+    provider: "Microsoft",
+    date: "Feb 2023",
+    badgeUrl: "/badges/microsoft-certified-azure-ai-fundamentals.png",
+    verificationLink: "https://www.credly.com/badges/e41626c1-8082-491c-a89e-391a0294e5dc/public_url"
+  },
+  {
+    id: "c7",
+    name: "CCNA: Introduction to Networks",
+    provider: "Cisco",
+    date: "Nov 2023",
+    badgeUrl: "/badges/cisco_logo.jpeg",
   }
+  //{
+    //id: "c8",
+    //name: "LangChain & LLM App Dev",
+    //provider: "DeepLearning.AI",
+    //date: "2023",
+    //badgeUrl: "https://ui-avatars.com/api/?name=L+C&background=16a34a&color=fff&size=128&bold=true",
+    //verificationLink: "https://www.deeplearning.ai/short-courses/langchain-for-llm-application-development/"
+  //}
 ];
 
 export const PROJECTS: Project[] = [
@@ -152,9 +183,30 @@ export const PROJECTS: Project[] = [
 
 export const PUBLICATIONS: Publication[] = [
   {
+    title: "Designing Sleepzy: A Mobile Interface for Lucid Dream Induction via Smart Pillow Technology",
+    conference: "18th International Research Conference - General Sir John Kotelawala Defence University",
+    date: "Oct 2025",
+    description: "Proposed a data-driven, user-centered mobile application design (Sleepzy) with a Personalized Adaptive Sleep Intervention (PASI) framework to non-invasively detect REM sleep and customize sensory cues for lucid dream induction and sleep disorder management.",
+    link: "https://irc.kdu.ac.lk/2025/documents/2025/abstracts/irc25_abstract_book_foc.pdf"
+  },
+  {
+    title: "A Unified Framework for SLA-Aware and Carbon-Conscious Green Cloud Computing: A Holistic Approach to Sustainable",
+    conference: "18th International Research Conference - General Sir John Kotelawala Defence University",
+    date: "Oct 2025",
+    description: "Developed the Unified Green Cloud Computing Optimization Framework (UGCCOF) to holistically balance energy efficiency, carbon emissions, and Service Level Agreements (SLAs) using a multi-objective decision-making model to overcome the limitations of fragmented single-metric solutions.",
+    link: "https://irc.kdu.ac.lk/2025/documents/2025/abstracts/irc25_abstract_book_foc.pdf"
+  },
+  {
+    title: "Enhancing Sustainability Awareness Through Mobile Applications: A Comparative Case Study and Analysis",
+    conference: "18th International Research Conference - General Sir John Kotelawala Defence University",
+    date: "Oct 2025",
+    description: "Comprehensive qualitative analysis of mobile applications (SDG and non-SDG based) to identify critical technical and UI/UX gaps hindering user retention and real-world impact on environmental sustainability",
+    link: "https://irc.kdu.ac.lk/2025/documents/2025/abstracts/irc25_abstract_book_fmsh.pdf"
+  }
+  /*{
     title: "Enhancing RAG Accuracy with Domain-Specific Ontologies",
-    conference: "National Undergraduate Research Symposium 2024",
-    date: "Dec 2024",
+    conference: "18th International Research Conference - General Sir John Kotelawala Defence University",
+    date: "Oct 2025",
     description: "A comparative study on how Knowledge Graphs outperform vector-only retrieval for complex reasoning tasks in medical datasets.",
     link: "https://example.com/publication/rag-ontologies" // Placeholder
   },
@@ -164,7 +216,7 @@ export const PUBLICATIONS: Publication[] = [
     date: "Aug 2024",
     description: "Survey paper analyzing the integration of machine learning models to predict and adapt haptic feedback intensity based on user immersion levels.",
     link: "https://example.com/publication/haptic-feedback" // Placeholder
-  }
+  }*/
 ];
 
 export const ACHIEVEMENTS: Achievement[] = [
